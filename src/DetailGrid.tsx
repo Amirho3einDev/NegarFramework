3c6a511266e18d456d3419d3569039687e68eb0f
Amirho3einDev <52045353+Amirho3einDev@users.noreply.github.com>
2024-11-29 18:51:18 +0330

    Change In Detail grid UI

--- src/DetailGrid.tsx
+++ src/DetailGrid.tsx
@@ -1,6 +1,9 @@
-import React, { Component } from 'react';
-import { Dialog } from 'primereact/dialog';
-import Form from './Form';
+import React, { Component } from "react";
+import { Dialog } from "primereact/dialog";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { Button } from "primereact/button";
+import Form from "./Form";
 
 interface DetailGridProps {
   model: any;
@@ -26,7 +29,7 @@ class DetailGrid extends Component<DetailGridProps, DetailGridState> {
 
   initializeNewDetail = (model: any) => {
     return model.fields.reduce((acc: any, field: any) => {
-      acc[field.name] = '';
+      acc[field.name] = "";
       return acc;
     }, {});
   };
@@ -43,53 +46,90 @@ class DetailGrid extends Component<DetailGridProps, DetailGridState> {
     this.props.onDelete(index);
   };
 
+  renderActions = (_: any, { rowIndex }: any) => {
+    return (
+      <Button
+        icon="pi pi-trash"
+        type="button" // جلوگیری از رفتار submit
+        className="p-button-rounded p-button-outlined p-button-danger"
+        onClick={() => this.handleDelete(rowIndex)}
+        tooltip="Delete"
+      />
+    );
+  };
+
   render() {
     const { model, data } = this.props;
     const { showDialog, newDetail } = this.state;
 
     return (
-      <div>
-        <table className="table">
-          <thead>
-            <tr>
-              {model.fields.map(
-                (field: any) =>
-                  field.visible && <th key={field.name}>{field.label}</th>
-              )}
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((row, index) => (
-              <tr key={index}>
-                {model.fields.map(
-                  (field: any) =>
-                    field.visible && <td key={field.name}>{row[field.name]}</td>
-                )}
-                <td>
-                  <button
-                    className="btn btn-danger"
-                    onClick={() => this.handleDelete(index)}
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <div className="p-card p-shadow-3 p-p-3">
+        <h3 className="p-text-secondary" style={{ marginBottom: "20px" }}>
+          Detail Grid
+        </h3>
 
-        <button
-          className="btn btn-primary mt-2"
-          onClick={() => this.setState({ showDialog: true })}
+        {/* دکمه افزودن جزئیات */}
+        <div className="p-d-flex p-jc-end p-mt-3">
+          <Button
+            label="Add Detail"
+            icon="pi pi-plus"
+            className="p-button-primary"
+            type="button" // جلوگیری از رفتار submit
+            style={{ width: "150px" }}
+            onClick={() => this.setState({ showDialog: true })}
+          />
+        </div>
+
+        {/* جدول داده‌ها */}
+        <DataTable
+          value={data}
+          className="p-datatable-sm p-datatable-gridlines p-datatable-striped"
+          responsiveLayout="scroll"
+          // paginator
+          // rows={5}
         >
-          Add Detail
-        </button>
+          {model.fields.map(
+            (field: any) =>
+              field.visible && (
+                <Column
+                  key={field.name}
+                  field={field.name}
+                  header={field.label}
+                  style={{ textAlign: "center" }}
+                />
+              )
+          )}
+          <Column
+            body={this.renderActions}
+            header="Actions"
+            style={{ width: "100px", textAlign: "center" }}
+          />
+        </DataTable>
 
+
+        {/* دیالوگ افزودن جزئیات */}
         <Dialog
-          header="Add Detail"
+          header={
+            <div className="p-d-flex p-ai-center p-jc-between">
+              <span style={{ fontSize: "18px", fontWeight: "bold" }}>
+                Add New Detail
+              </span>
+            </div>
+          }
           visible={showDialog}
+          style={{ width: "50vw" }}
+          modal
           onHide={() => this.setState({ showDialog: false })}
+          footer={
+            <div className="p-d-flex p-jc-end">
+              <Button
+                label="Close"
+                icon="pi pi-times"
+                className="p-button-text"
+                onClick={() => this.setState({ showDialog: false })}
+              />
+            </div>
+          }
         >
           <Form
             model={model}
